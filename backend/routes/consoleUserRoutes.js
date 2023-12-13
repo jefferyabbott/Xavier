65f6899a4a692a4a439be8ba4ecc422babcf1a2c
Jeffery Abbott <jeff@jefferyabbott.com>
2023-12-13 05:11:53 -1000

    Modify user registration pattern

--- backend/routes/consoleUserRoutes.js
+++ backend/routes/consoleUserRoutes.js
@@ -2,7 +2,10 @@ import { Router } from 'express';
 const router = Router();
 import { registerUser, loginUser } from '../controllers/userController.js';
 
-router.post('/', registerUser);
+// uncomment the register route when creating the first user account
+// after the first user account is created, comment out the register route to prevent people from creating their own accounts
+// router.post('/register', registerUser);
+
 router.post('/login', loginUser);
 
 export default router;
